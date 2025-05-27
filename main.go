534c0a1d806bee468d42c13dc47e524157d89c7e
Shah1011 <officiallyshah@outlook.com>
2025-05-27 16:25:54 +0530

    feat: ls cmd implementation

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"log"
+
+	"github.com/joho/godotenv"
 	"github.com/shah1011/obscure/cmd"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("⚠️  .env file not found or couldn't be loaded. Falling back to default env vars.")
+	}
 	cmd.Execute()
 }
